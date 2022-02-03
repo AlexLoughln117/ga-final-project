ca029fae47cb1f95b89bd57a2338005312d3d67b
Alex Loughlin <alex@drewlondon.co.uk>
2022-02-03 17:41:36 +0000

    Adding cors anywhere back in

--- js/giantbomb.js
+++ js/giantbomb.js
@@ -1,7 +1,7 @@
 export const fetchGiantBomb = async () => {
   try {
     const response = await fetch(
-      'https://www.giantbomb.com/api/games/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json',
+      'https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/games/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json',
     );
     //https://www.giantbomb.com/api/characters/?api_key=[YOUR API KEY]
     // To pull more games paginate the url http://www.giantbomb.com/api/games/?api_key=[APIKEY]&format=json&field_list=name&page=2
@@ -19,7 +19,7 @@ export const fetchGiantBomb = async () => {
 export const fetchGiantBombCharacters = async () => {
   try {
     const response = await fetch(
-      'https://www.giantbomb.com/api/characters/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json',
+      'https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/characters/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json',
     );
 
     if (response.ok) {
@@ -36,7 +36,7 @@ export const fetchMoreDetails = async (guid) => {
   console.log(guid);
   try {
     const response = await fetch(
-      `https://www.giantbomb.com/api/game/${guid}/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json`,
+      `https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/game/${guid}/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json`,
     );
 
     if (response.ok) {
@@ -53,7 +53,7 @@ export const searchGiantBomb = async (userText) => {
   // console.log(userText);
   try {
     const response = await fetch(
-      `https://www.giantbomb.com/api/search/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json&query=%22${userText}%22&resources=game`,
+      `https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/search/?api_key=5caa696b41ac3fc2a8a789a382f4337109e98366&format=json&query=%22${userText}%22&resources=game`,
     );
 
     if (response.ok) {
